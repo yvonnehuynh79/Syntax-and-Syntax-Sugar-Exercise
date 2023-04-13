3bc74039cabdc4f8534971fb3709d8720cf26e02
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-13 12:51:40 -0700

    first commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = "";
+
+
+
+            /* if ( answer < 9)
+
+            {
+
+                response = answer + " is less than nine";
+            }
+            else
+                {
+                response = answer + "greater than or equal to nine";
+
+                }*/
+
+            /*string interpolation;*/
+
+                if (answer < 9)
+            {
+                response = $"{answer}  is less than nine";
+            }
+            else
+            {
+                response = $"{answer} is greater than or equal to nine";
+            }
+            /*ternary operator */
+            Console.WriteLine((answer <9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine");
+
+
+
         }
     }
 }
